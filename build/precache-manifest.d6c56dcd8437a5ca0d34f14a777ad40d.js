535ee12a2a7ad442e809ef51bfdfe5752f39774a
esaharov <esaharov@osslabs.ru>
2019-07-08 12:54:18 +0400

    lessonsR9

---
+++ build/precache-manifest.d6c56dcd8437a5ca0d34f14a777ad40d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96533f63b25cf2ed365894ec573b9e00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d15005da10c3ee391b26",
+    "url": "/static/css/main.6c203507.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "d15005da10c3ee391b26",
+    "url": "/static/js/main.2041cca6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 1
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;

ee4c9a0dba53c4f7191ba49ae526bb1aa390e9f2
esaharov <esaharov@osslabs.ru>
2019-07-08 13:39:09 +0400

    Lesson14R

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={s.header}>
             <img src="https://www.designevo.com/res/templates/thumb_small/brown-circle-and-chocolate-coffee.png" />
         </header>
     );

c04e6deb7bbd9389be0ac8c2b97223d24f8f30ea
evgenii <saharovpiib101@gmail.com>
2019-08-10 09:05:18 +0400

    61RLesson

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -10,8 +10,10 @@ class HeaderContainer extends Component {
         this.props.toggleIsFetching(false);
         axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true})
             .then((response) => {
-                let {id, login, email} = response.data.data;
-                this.props.setUserData(id, email, login);
+                if (response.data.resultCode === 0) {
+                    let {id, login, email} = response.data.data;
+                    this.props.setUserData(id, email, login);
+                }
                 this.props.toggleIsFetching(true);
             })
     }
